2692d1da4bde60fd7a153049af63c5633a606298
Sam Szreter <s.szreter@gmail.com>
2017-08-16 11:35:15 +0100

    Update SpeciesCommand

--- src/main/java/pokemanager/SpeciesCommand.java
+++ src/main/java/pokemanager/SpeciesCommand.java
@@ -1,20 +1,36 @@
 package pokemanager;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class SpeciesCommand implements Command {
-	private final SpeciesFinder speciesFinder;
+	private SpeciesFinder speciesFinder;
+	private PrintStream printStream;
 
 	public SpeciesCommand(SpeciesFinder speciesFinder) {
 		super();
 		this.speciesFinder = speciesFinder;
 	}
 
+    public SpeciesCommand(SpeciesFinder speciesFinder, PrintStream printStream) {
+        super();
+        this.speciesFinder = speciesFinder;
+        this.printStream = printStream;
+    }
+
     public boolean respondsTo(String command) {
 		return command.startsWith("search");
     }
     
-    public void execute(String command) throws IOException { return; }
+    public void execute(String command) throws IOException {
+        String name = command.substring(7);
+		Species species = speciesFinder.findDetails(name);
+        if (species == null) {
+            printStream.println("no species found");
+        } else {
+		    printStream.println(species.toString());
+        }
+    }
 
     public void execute(String command, App app) throws IOException {
         String name = command.substring(7);
--- src/test/java/pokemanager/SpeciesCommandTest.java
+++ src/test/java/pokemanager/SpeciesCommandTest.java
@@ -12,31 +12,29 @@ public class SpeciesCommandTest {
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     private final PrintStream printStream = new PrintStream(out);
 	private final BoxSpy box = new BoxSpy();
-    private final InputStream in = new ByteArrayInputStream("".getBytes());
-    private final App app = new App(in, printStream, box, null);
     private final SpeciesFinderSpy speciesFinder = new SpeciesFinderSpy();
-    private final SpeciesCommand sc = new SpeciesCommand(speciesFinder);
+    private final SpeciesCommand sc = new SpeciesCommand(speciesFinder, printStream);
 
     @Test
     public void CallsAndPrintsToStringOnReturnedSpecies() throws Exception {
-        sc.execute("search Bulbasaur", app);
+        sc.execute("search Bulbasaur");
         assertEquals("Name: Bulbasaur\nHeight: 7\nWeight: 69\n", out.toString());
     }
 
     @Test
     public void PrintsNoneFoundOnNullReturn() throws Exception {
-        sc.execute("search bad_input", app);
+        sc.execute("search bad_input");
         assertEquals("no species found\n", out.toString());
     }
     @Test
     public void CallsFindDetailsOnApiSearcher() throws Exception {
-        sc.execute("search Bulbasaur", app);
+        sc.execute("search Bulbasaur");
         assertTrue(speciesFinder.findDetailsCalled);
     }
 
 	@Test
 	public void PassesNameToApiSearcher() throws Exception {
-		sc.execute("search Bulbasaur", app);
+		sc.execute("search Bulbasaur");
 		assertEquals("Bulbasaur", speciesFinder.calledWith);
 	}
 
