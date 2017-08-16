eed8994be7884d9aeec7097079c53669d220fe8e
Sam Szreter <s.szreter@gmail.com>
2017-08-16 11:09:20 +0100

    Update SaveCommand

--- src/main/java/pokemanager/SaveCommand.java
+++ src/main/java/pokemanager/SaveCommand.java
@@ -3,10 +3,22 @@ package pokemanager;
 import java.io.*;
 
 public class SaveCommand implements Command {
+    private Box box;
+    private PrintStream printStream;
+
 	public SaveCommand() {
 	}
 
-    public void execute(String command) throws IOException { return; }
+    public SaveCommand(Box box, PrintStream printStream) {
+        this.box = box;
+        this.printStream = printStream;
+    }
+    
+    public void execute(String command) throws IOException {
+        box.save();
+        printStream.println("Saved!\n");
+    }
+
 	public void execute(String command, App app) throws IOException {
 		app.getBox().save();
 		app.getPrintStream().println("Saved!\n");
--- src/test/java/pokemanager/SaveCommandTest.java
+++ src/test/java/pokemanager/SaveCommandTest.java
@@ -12,34 +12,25 @@ public class SaveCommandTest {
 	
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     private final PrintStream printStream = new PrintStream(out);
-	private final BoxSpy box = new BoxSpy();
-	private final SaveCommand sc = new SaveCommand();
-    private final InputStream in = new ByteArrayInputStream("".getBytes());
-    private final File tempFile = File.createTempFile("temp-", "-testfile");
-    private final App app = new App(in, printStream, box, null);
+	private final BoxSpy boxSpy = new BoxSpy();
+	private final SaveCommand sc = new SaveCommand(boxSpy, printStream);
 
 	public SaveCommandTest() throws IOException {}
 
 	@Test
 	public void CallsSaveOnBox() throws Exception {
-		sc.execute("save", app);
-		assertTrue(box.saveCalled);
+		sc.execute("save");
+		assertTrue(boxSpy.saveCalled);
 	}
 
 	@Test
 	public void PrintsSaved() throws Exception {
-		sc.execute("save", app);
-		assertEquals(out.toString(), "Saved!\n\n");
+		sc.execute("save");
+		assertEquals("Saved!\n\n", out.toString());
 	}	
 
 	@Test
 	public void RespondsToSave() throws Exception {
 		assertTrue(sc.respondsTo("save"));
 	}
-
-    public String inputStreamToString(InputStream inputStream) {
-        String result = new BufferedReader(new InputStreamReader(inputStream))
-                .lines().collect(Collectors.joining("\n"));
-        return result;
-    }
 }	
